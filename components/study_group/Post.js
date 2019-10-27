f9971059d7aacca145c59e786990ec3daf20af5f
binlong09 <binlong09@gmail.com>
2019-10-27 16:26:15 -0400

    added post boilerplate component

--- components/study_group/Post.js
+++ components/study_group/Post.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, Stylesheet, View, TouchableOpacity, Text} from 'react-native';
+import { Image, Stylesheet, View, TouchableOpacity, Text } from 'react-native';
 
 export default class Post extends Component {
   constructor(props) {
@@ -16,3 +16,7 @@ export default class Post extends Component {
     )
   }
 }
+
+const stypes = StyleSheet.create({
+
+})
